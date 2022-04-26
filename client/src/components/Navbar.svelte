1e39b87219b7d30312527050838b50358b930d10
Magnus <752zombie@gmail.com>
2022-04-26 17:59:36 +0200

    basic sign in and sign out functionality

--- client/src/components/Navbar.svelte
+++ client/src/components/Navbar.svelte
@@ -1,8 +1,54 @@
 <script>
     import { Router, Link, Route } from "svelte-navigator";
+    import {loginStore} from "../stores.js";
+    import signOut from "../scripts/signout.js"
+    import {navigate} from "svelte-navigator";
+
+    let isLoggedIn = false;
+    loginStore.subscribe((value) => isLoggedIn = value);
 </script>
 
 <nav class="link-wrapper">
     <Link to="/" class="header-link space-between first-link">Home</Link>
-    <Link to="/home2" class="header-link space-between first-link">Hom2</Link>
+    <Link to="/posts" class="header-link align-left">Posts</Link>
+    
+    {#if !isLoggedIn}
+    <Link to="login" class="header-link align-right">Sign in</Link>
+    {:else}
+    <span class="header-link align-right" on:click={() => signOut(navigate)}>Sign out</span>
+    {/if}
 </nav>
+
+<style>
+    nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	nav :global(.align-right) {
+		margin-right: 20px;
+	}
+
+	nav :global(.align-left) {
+		margin-right: auto;
+	}
+
+	nav :global(.first-link) {
+		margin-left: 10px;
+		margin-right: 20px;
+	}
+	
+	nav {
+		background-color: rgb(72, 72, 199);
+		padding-top: 25px;
+		padding-bottom: 25px;
+	}
+
+	nav :global(.header-link) {
+		text-decoration: none;
+		color: white;
+		font-size: x-large;
+        cursor: pointer;
+	}
+
+</style>
