0f2c71a329db05f8bbeb5da12b77dbea75e67172
Magnus <752zombie@gmail.com>
2022-04-27 19:51:37 +0200

    trying to make socket.io work on frontend

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,11 +1,38 @@
+
+
 <script>
-    
+    import { onMount } from "svelte";
+
+    let socketReady = false;
+    let mounted = false;
+    let socket;
+
+    onMount(() => {
+        mounted = true;
+        if (socketReady) {
+            loadSocket();
+        }
+    })
+
+    function socketLoaded() {
+        socketReady = true;
+        if (mounted) {
+            loadSocket();
+        }
+    }
+
+    function loadSocket() {
+        socket = io();
+    }
+
 </script>
+
+<svelte:head>
+    <script src="/socket.io/socket.io.js" on:load={socketLoaded}></script>
+</svelte:head>
 
 <div>
     <h1>COLOR</h1>
-    home
-    lol
-    hehe
     
+    <button on:click={socket.emit("chat message", "hej med dig")}>Add message</button>
 </div>
