4496aade2c374ba98f4e3417508d34d28a44506d
Magnus <752zombie@gmail.com>
2022-06-16 22:52:30 +0200

    good code

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -184,6 +184,19 @@ function sortByLikes() {
            "LIMIT 5 OFFSET ?"
 }
 
+function sortBy(sortOr) {
+    return "SELECT p.id, p.title, p.text, p.photo, p.like, p.date, u.username, " + 
+           "ifnull(l.user_id, 0) as liked, COUNT(comment) as comment_count, COUNT(reply) as reply_count " +    
+           "FROM posts as p " + 
+           "LEFT JOIN post_like_user as l on l.post_id = p.id " +
+           "LEFT JOIN comments as c on c.post_id = p.id " +
+           "LEFT JOIN replies as r on r.comment_id = c.id " +
+           "INNER JOIN users as u on u.id = p.user_id " +  
+           "GROUP BY p.id " +
+           "ORDER BY " + sortOrder + " DESC " + 
+           "LIMIT 5 OFFSET ?"
+}
+
 
 // Delete Post
 router.delete("/post", async (req, res) => {
