016f02a06a2f959c9d2bf9ee1135e4e9bf03e479
Magnus <752zombie@gmail.com>
2022-06-17 13:16:57 +0200

    removed duplicate json body parser

--- server/app.js
+++ server/app.js
@@ -20,7 +20,6 @@ const io = new Server(httpServer);
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-app.use(express.json());
 
 const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET,
